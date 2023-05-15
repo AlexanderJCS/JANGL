812fbd819eb0e031ae444ae9833d2f21829bd132
Alex <alexandercastronovo@gmail.com>
2023-05-14 23:58:53 -0400

    Overload Texture constructor: you can now select a portion of the image to load

--- src/main/java/jangl/graphics/Texture.java
+++ src/main/java/jangl/graphics/Texture.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
 
 import static org.lwjgl.opengl.GL21.*;
 
@@ -18,47 +19,85 @@ public class Texture implements AutoCloseable {
     private final int id;
 
     /**
-     * Creates a new Texture object.
-     *
      * @param filepath The filepath of the texture.
      */
-
     public Texture(String filepath) throws UncheckedIOException {
         try {
             BufferManager.bufferedImage = ImageIO.read(new File(filepath));
-            int width = BufferManager.bufferedImage.getWidth();
-            int height = BufferManager.bufferedImage.getHeight();
-
-            int[] pixels_raw;
-            pixels_raw = BufferManager.bufferedImage.getRGB(0, 0, width, height, null, 0, width);
-
-            // Create the pixel buffer
-            BufferManager.BYTE_BUFFER.clear();
-            BufferManager.BYTE_BUFFER.limit(width * height * 4);
-
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    int pixel = pixels_raw[i * width + j];
-                    BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 16) & 0xFF));  // Red
-                    BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 8) & 0xFF));   // Green
-                    BufferManager.BYTE_BUFFER.put((byte) (pixel & 0xFF));          // Blue
-                    BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 24) & 0xFF));  // Alpha
-                }
-            }
-
-            BufferManager.BYTE_BUFFER.flip();
-            id = glGenTextures();
-            this.bind();
-
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, BufferManager.BYTE_BUFFER);
-            Texture.unbind();
-
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+
+        int width = BufferManager.bufferedImage.getWidth();
+        int height = BufferManager.bufferedImage.getHeight();
+
+        int[] rawData = BufferManager.bufferedImage.getRGB(0, 0, width, height, null, 0, width);
+        this.calculateImageData(rawData, width, height);
+
+        this.id = this.createImage(width, height);
+    }
+
+    /**
+     * @param filepath The filepath of the image.
+     * @param x The x component, in pixels, of the top left corner of the rectangular region of the image to get
+     * @param y The y component, in pixels, of the top left corner of the rectangular region of the image to get
+     * @param width The width of the region, in pixels, to get.
+     * @param height The height of the region, in pixels, to get.
+     *
+     * @throws IndexOutOfBoundsException Throws IndexOutOfBoundsException if the specified rectangle goes off the image
+     */
+    public Texture(String filepath, int x, int y, int width, int height) throws IndexOutOfBoundsException {
+        try {
+            BufferManager.bufferedImage = ImageIO.read(new File(filepath));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        int[] rawData = BufferManager.bufferedImage.getRGB(x, y, width, height, null, 0, width);
+        this.calculateImageData(rawData, width, height);
+
+        this.id = this.createImage(width, height);
+    }
+
+    /**
+     * Writes the designated region of image data to BufferManager.BYTE_BUFFER
+     *
+     * @param rawData The raw image data
+     * @param x The top left x coord of the region to get
+     * @param y The top left y coord of the region to get
+     * @param width The width of the region to get
+     * @param height The height of the region to get
+     */
+    private void calculateImageData(int[] rawData, int width, int height) {
+        BufferManager.BYTE_BUFFER.clear();
+        BufferManager.BYTE_BUFFER.limit(width * height * 4);
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int pixel = rawData[i * width + j];
+                BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 16) & 0xFF));  // Red
+                BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 8) & 0xFF));   // Green
+                BufferManager.BYTE_BUFFER.put((byte) (pixel & 0xFF));          // Blue
+                BufferManager.BYTE_BUFFER.put((byte) ((pixel >> 24) & 0xFF));  // Alpha
+            }
+        }
+
+        BufferManager.BYTE_BUFFER.flip();
+    }
+
+    /**
+     * @return the ID of the created image
+     */
+    private int createImage(int width, int height) {
+        int imageID = glGenTextures();
+
+        glBindTexture(GL_TEXTURE_2D, imageID);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, BufferManager.BYTE_BUFFER);
+        Texture.unbind();
+
+        return imageID;
     }
 
     /**
