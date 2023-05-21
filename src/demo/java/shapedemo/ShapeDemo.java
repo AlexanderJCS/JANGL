cd0b8b6888e6e7a1f65f3e7219098af16787a17c
Alex <alexandercastronovo@gmail.com>
2023-05-20 22:47:34 -0400

    Refactor ShapeDemo

--- src/demo/java/shapedemo/ShapeDemo.java
+++ src/demo/java/shapedemo/ShapeDemo.java
@@ -10,60 +10,74 @@ import jangl.shapes.Shape;
 import jangl.time.Clock;
 
 public class ShapeDemo {
+    private final Rect rect;
+    private final Circle circle;
+    private final ColorShader colorShader;
+    private final Rect background;
+
     public ShapeDemo() {
-        JANGL.init(1600, 900);
+        this.rect = new Rect(new ScreenCoords(-0.25f, 0.25f), 0.5f, 0.5f);
+        this.circle = new Circle(new ScreenCoords(0, 0.5f), 0.1f, 70);
+        this.colorShader = new ColorShader(1, 0, 0, 1);
+        this.background = new Rect(new ScreenCoords(-1, 1), 2, 2);
+    }
+
+    public void draw() {
+        // Make the screen black for the next frame
+        Window.clear();
+
+        // Draw the background using the given color shader
+        this.background.draw(this.colorShader);
+
+        // Draw the rectangle and circle
+        // These draw calls need to be after the background so the background doesn't overlap the shapes
+        this.rect.draw();
+        this.circle.draw();
+    }
+
+    public void update() {
+        // Update JANGL so events can be received and the screen doesn't say "not responding"
+        JANGL.update();
+
+        // Rotate the rectangle and circle 0.01 radians across the center of the screen every second
+        this.rect.rotateAxis(0.25 * Clock.getTimeDelta());
+        this.circle.rotateAxis(0.05 * Clock.getTimeDelta());
+
+        // Draw a green background if the rectangle collides with the circle
+        // Otherwise draw a red background
+        if (Shape.collides(this.rect, this.circle)) {
+            // Set the color to 0 red, 1, green, 0 blue, 1 alpha (0 transparency)
+            this.colorShader.setRGBA(0, 0.8f, 0, 1);
+        } else {
+            // Set the color to 1 red, 0, green, 0 blue, 1 alpha (0 transparency)
+            this.colorShader.setRGBA(0.8f, 0, 0, 1);
+        }
     }
 
     public void run() {
-        // Create a rect and a circle using a try with resources statement.
-        // If a shape is not closed, (either manually or with the try with resources) a memory leak will occur.
-        try (
-                Rect rect = new Rect(new ScreenCoords(-0.25f, 0.25f), 0.5f, 0.5f);
-                Circle circle = new Circle(new ScreenCoords(0, 0.5f), 0.1f, 70);
-                ColorShader colorShader = new ColorShader(1, 0, 0, 1);
-                Rect background = new Rect(new ScreenCoords(-1, 1), 2, 2)
-        ) {
-            // While the "X" button on the top right of the window is not pressed
-            while (Window.shouldRun()) {
-                // Draw a green background if the rectangle collides with the circle
-                // Otherwise draw a red background
-                if (Shape.collides(rect, circle)) {
-                    // Set the color to 0 red, 1, green, 0 blue, 1 alpha (0 transparency)
-                    colorShader.setRGBA(0, 0.8f, 0, 1);
-                } else {
-                    // Set the color to 1 red, 0, green, 0 blue, 1 alpha (0 transparency)
-                    colorShader.setRGBA(0.8f, 0, 0, 1);
-                }
+        // While the "X" button on the top right of the window is not pressed
+        while (Window.shouldRun()) {
+            this.update();
+            this.draw();
 
-                // Make the screen black for the next frame
-                Window.clear();
-
-                // Draw the background using the given color shader
-                background.draw(colorShader);
-
-                // Draw the rectangle and circle
-                // These draw calls need to be after the background so the background doesn't overlap the shapes
-                rect.draw();
-                circle.draw();
-
-                // End your draw method with JANGL.update()
-                JANGL.update();
-
-                // Rotate the rectangle and circle 0.01 radians across the center of the screen every second
-                rect.rotateAxis(0.25 * Clock.getTimeDelta());
-                circle.rotateAxis(0.05 * Clock.getTimeDelta());
-
-                // Tick the clock so the FPS is equal to 60
-                Clock.busyTick(60);
-                Window.setTitle("JANGL | FPS: " + Math.round(Clock.getSmoothedFps() * 10) / 10f);
-            }
+            // Tick the clock so the FPS is equal to 60
+            Clock.busyTick(60);
         }
 
         // Close the window when the program is done running
         Window.close();
+
+        // Close all shapes and textures the program used. This is important in order to prevent memory leaks
+        this.rect.close();
+        this.circle.close();
+        this.colorShader.close();
+        this.background.close();
     }
 
     public static void main(String[] args) {
+        // Initialize JANGL with a screen width of 1600 pixels and a screen height of 900 pixels
+        JANGL.init(1600, 900);
+
         new ShapeDemo().run();
     }
 }
