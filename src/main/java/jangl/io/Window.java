1306e9474ffefd5c005ed8d1393982ae24ed30ab
Alex <alexandercastronovo@gmail.com>
2023-07-31 23:52:30 -0400

    Re-enable antialiasing

--- src/main/java/jangl/io/Window.java
+++ src/main/java/jangl/io/Window.java
@@ -39,6 +39,7 @@ public class Window {
         initialized = true;
 
         glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);  // make the window non-resizeable
+        glfwWindowHint(GLFW_SAMPLES, 4);
 
         window = glfwCreateWindow(screenWidth, screenHeight, "JANGL", 0, 0);
         glfwShowWindow(window);
@@ -47,6 +48,7 @@ public class Window {
         GL.createCapabilities();
 
         glEnable(GL_TEXTURE_2D);
+        glEnable(GL_MULTISAMPLE);
 
         Window.setVsync(false);
     }
