a9e133629a5b25851ba135daaf979f5ab488a577
Alex <alexandercastronovo@gmail.com>
2023-08-07 19:26:18 -0400

    Update TextureDemo to show different shapes

--- src/demo/java/texturedemo/TextureDemo.java
+++ src/demo/java/texturedemo/TextureDemo.java
@@ -5,19 +5,23 @@ import jangl.coords.WorldCoords;
 import jangl.graphics.textures.Texture;
 import jangl.graphics.textures.TextureBuilder;
 import jangl.io.Window;
+import jangl.shapes.Circle;
 import jangl.shapes.Rect;
+import jangl.shapes.Triangle;
 
 public class TextureDemo implements AutoCloseable {
-    private final Rect rect1;
-    private final Rect rect2;
+    private final Rect rect;
+    private final Circle circle;
+    private final Triangle triangle;
     private final Texture texture;
 
     public TextureDemo() {
-        this.rect1 = new Rect(new WorldCoords(0, 0.5f), 0.5f, 0.5f);
-        this.rect2 = new Rect(new WorldCoords(0.7f, 0.5f), 0.5f, 0.5f);
+        this.rect = new Rect(new WorldCoords(0.8f, 0.6f), 0.25f, 0.25f);
+        this.circle = new Circle(new WorldCoords(1.3f, 0.5f), 0.1f, 70);
+        this.triangle = new Triangle(new WorldCoords(0.3f, 0.3f), new WorldCoords(0.8f, 0.3f), new WorldCoords(0.5f, 0.6f));
 
-        this.rect1.setTexRepeatX(2);
-        this.rect1.setTexRepeatY(2);
+        this.rect.setTexRepeatX(2);
+        this.rect.setTexRepeatY(2);
 
         this.texture = new Texture(
                 new TextureBuilder().setImagePath("src/demo/demoResources/textureDemo/image.png")
@@ -27,8 +31,9 @@ public class TextureDemo implements AutoCloseable {
     public void draw() {
         Window.clear();
 
-        this.rect1.draw(this.texture);
-        this.rect2.draw(this.texture);
+        this.rect.draw(this.texture);
+        this.circle.draw(this.texture);
+        this.triangle.draw(this.texture);
     }
 
     public void run() {
@@ -41,8 +46,9 @@ public class TextureDemo implements AutoCloseable {
 
     @Override
     public void close() {
-        this.rect1.close();
-        this.rect2.close();
+        this.rect.close();
+        this.circle.close();
+        this.triangle.close();
         this.texture.close();
     }
 
