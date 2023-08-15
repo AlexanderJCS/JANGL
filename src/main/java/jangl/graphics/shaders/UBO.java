385dd0e660ef25e8dd8f8610a7feceaed6d7c6e7
AlexanderJCS <alexandercastronovo@gmail.com>
2023-08-15 11:20:21 -0400

    UBO allows binding points up to UBO.getMaxBindingPoint()

--- src/main/java/jangl/graphics/shaders/UBO.java
+++ src/main/java/jangl/graphics/shaders/UBO.java
@@ -24,11 +24,9 @@ public class UBO implements Bindable, AutoCloseable {
      * @throws IllegalArgumentException If the bindingPoint is greater than 84, less than 0, or equal to an already existing binding point.
      */
     public UBO(float[] data, int bindingPoint) throws IllegalArgumentException {
-        if (bindingPoint < 0 || bindingPoint > 74) {
+        if (bindingPoint < 0 || bindingPoint > UBO.getMaxBindingPoint()) {
             throw new IllegalArgumentException(
-                    "Binding point must be within the range of [0, 74]. In Windows and Linux," +
-                    "the binding point may go up to 83 (inclusive), but to be cross compatible, JANGL does not allow" +
-                    "you to have a binding point greater than 74 (inclusive)."
+                    "Binding point " + bindingPoint + " is not within the range of [0, UBO.getMaxBindingPoint()] (inclusive)."
             );
         }
 
