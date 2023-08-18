c481579537fced179dea72dc7d05abd09cf07631
Alex <alexandercastronovo@gmail.com>
2023-08-17 22:32:44 -0400

    Add text to IconDemo explaining it

--- src/demo/java/demos/icondemo/IconDemo.java
+++ src/demo/java/demos/icondemo/IconDemo.java
@@ -1,6 +1,10 @@
 package demos.icondemo;
 
 import jangl.JANGL;
+import jangl.coords.WorldCoords;
+import jangl.graphics.font.Font;
+import jangl.graphics.font.Justify;
+import jangl.graphics.font.Text;
 import jangl.graphics.textures.TextureBuilder;
 import jangl.io.Window;
 
@@ -8,11 +12,24 @@ import jangl.io.Window;
  * This demo tests the customization of the mouse cursor and the window desktop icon.
  * The desktop icon does not set properly on macOS because GLFW does not support it.
  */
-public class IconDemo {
+public class IconDemo implements AutoCloseable {
     TextureBuilder texture;
+    private final Text text;
 
     public IconDemo() {
         this.texture = new TextureBuilder().setImagePath("src/demo/demoResources/iconDemo/circle.png");
+
+        Font arial = new Font(
+                "src/demo/demoResources/font/arial.fnt",
+                "src/demo/demoResources/font/arial.png"
+        );
+
+        this.text = new Text(
+                WorldCoords.getMiddle(),
+                arial, 0.05f,
+                "Notice how the cursor and the window icon changed.\nThe window icon does not change on macOS.",
+                Justify.CENTER
+        );
 
         Window.setIcon(this.texture);
         Window.setCursor(this.texture);
@@ -21,8 +38,16 @@ public class IconDemo {
     public void run() {
         while (Window.shouldRun()) {
             Window.clear();
+
+            this.text.draw();
+
             JANGL.update();
         }
+    }
+
+    @Override
+    public void close() {
+        this.text.close();
     }
 
     public static void main(String[] args) {
