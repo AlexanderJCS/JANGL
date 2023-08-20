cb5d24f1680b0887c9a85b30decb3f583bbb2c1f
Alex <alexandercastronovo@gmail.com>
2023-08-19 21:45:46 -0400

    Add .gradle to gitignore

---
+++ src/test/java/guides/imageguide/ImageGuide.java
@@ -0,0 +1,57 @@
+package guideCode.guides.imageguide;
+
+import jangl.JANGL;
+import jangl.coords.WorldCoords;
+import jangl.graphics.textures.Image;
+import jangl.graphics.textures.Texture;
+import jangl.graphics.textures.TextureBuilder;
+import jangl.io.Window;
+import jangl.shapes.Rect;
+
+public class ImageGuide {
+    private final Image image;
+
+    public ImageGuide() {
+        this.image = new Image(
+                new Rect(
+                        new WorldCoords(0.5f, 0.5f), 0.3f, 0.3f
+                ),
+
+                new Texture(
+                        new TextureBuilder().setImagePath("src/guideCode/guideResources/imageGuide/image.png")
+                )
+        );
+
+        this.image.rect().setTexRepeatX(2);
+        this.image.rect().setTexRepeatY(2);
+    }
+
+    public void draw() {
+        Window.clear();
+
+        // Draw the image
+        this.image.draw();
+    }
+
+    public void run() {
+        while (Window.shouldRun()) {
+            this.draw();
+
+            // This is method is required to be called so the window doesn't say "not responding"
+            JANGL.update();
+        }
+
+        this.image.rect().close();
+        this.image.texture().close();
+    }
+
+    public static void main(String[] args) {
+        // Initialize the window with the width of 1600 pixels and the height of 900 pixels
+        JANGL.init(1600, 900);
+        Window.setVsync(true);
+
+        new ImageGuide().run();
+
+        Window.close();
+    }
+}
---
+++ src/test/java/guides/quickstart/Quickstart.java
@@ -0,0 +1,40 @@
+package guideCode.guides.quickstart;
+
+import jangl.JANGL;
+import jangl.coords.WorldCoords;
+import jangl.io.Window;
+import jangl.shapes.Rect;
+
+public class Quickstart implements AutoCloseable {
+    private final Rect rect;
+
+    public Quickstart() {
+        this.rect = new Rect(new WorldCoords(0.5f, 0.75f), 0.5f, 0.5f);
+    }
+
+    public void run() {
+        while (Window.shouldRun()) {
+            JANGL.update();
+            Window.clear();
+
+            this.rect.draw();
+        }
+    }
+
+    @Override
+    public void close() {
+        this.rect.close();
+    }
+
+    public static void main(String[] args) {
+        // Input the width and height of your screen in pixels.
+        JANGL.init(1600, 900);
+        Window.setVsync(true);  // turn vsync on
+
+        Quickstart quickstart = new Quickstart();
+        quickstart.run();
+        quickstart.close();
+
+        Window.close();
+    }
+}
